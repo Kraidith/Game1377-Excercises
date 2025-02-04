33d1fa813c2e64f13e9c3fd0ec1b58cabe23e871
Logan Winscher <logan.winscher@g.austincc.edu>
2025-02-03 23:29:34 -0600

    Add project files.

---
+++ Game1377 Excercises3/Exercise 3.cs
@@ -0,0 +1,42 @@
+﻿namespace Game1377_Excercises3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing Main\n");
+            OutgoingStrings();
+            Arithmetic();
+            Assignment();
+            ComparisonAndConditionals();
+        }
+        static void OutgoingStrings()
+        {
+            string Game1377 = "I'm in Game 1377: Scripting for Game Designers.";
+            Console.WriteLine(Game1377);
+            Console.WriteLine(Game1377 + "\n");
+            Console.WriteLine(Game1377.ToUpper());
+            Console.WriteLine(Game1377.ToLower());
+            Console.WriteLine(Game1377.Substring(7));
+            Console.WriteLine(Game1377.Substring(7,4));
+            Console.WriteLine(Game1377.Substring(7,1));
+        }
+        static void Arithmetic()
+        {
+
+        }
+        static void Assignment()
+        {
+
+        }
+
+
+        static void ComparisonAndConditionals()
+        {
+
+        }
+
+
+    }
+}
+
