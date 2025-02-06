772b69b3ebaf74cbaa6f8d45f171f527496cf397
Logan Winscher <logan.winscher@g.austincc.edu>
2025-02-06 17:42:53 -0600

    declared and wrote functions

---
+++ HW 2/Exercise4.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace HW_2
+{
+    internal class Exercise4
+    {
+        static void Main(string[] args)
+        {
+         Switch();
+         ArraysAndLoops();
+         Methods();
+        }
+
+        private static void Switch()
+        {
+
+        }
+
+        private static void ArraysAndLoops()
+        {
+
+        }
+
+        private static void Methods()
+        {
+
+        }
+    }
+}
